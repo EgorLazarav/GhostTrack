4e1cb94e2e508da337a7658c3330f178fcfd7bd5
LuQmu5 <luqmu5.1007@gmail.com>
2023-07-01 10:55:55 +0400

    1

--- Assets/Scripts/Enemy/StateMachine/PatrolState.cs
+++ Assets/Scripts/Enemy/StateMachine/PatrolState.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 public class PatrolState : State
 {
     [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private float _patrolRangeX = 4;
+    [SerializeField] private float _patrolRangeY = 4;
+    [SerializeField] private float _maxStopTime = 6;
 
+    private Rect _patrolArea;
     private Coroutine _coroutine;
-    private Vector3 _startPosition;
 
     private void OnEnable()
     {
@@ -16,29 +20,35 @@ public class PatrolState : State
 
     private void OnDisable()
     {
-        _agent.isStopped = true;
         StopCoroutine(_coroutine);
     }
 
     private void Start()
     {
-        _startPosition = transform.position;
+        _patrolArea = new Rect(transform.position.x, transform.position.y, _patrolRangeX, _patrolRangeY);
     }
 
     private IEnumerator Patroling()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(3, 6));
+            yield return new WaitForSeconds(Random.Range(0, _maxStopTime));
 
-            Vector3 point = new Vector3(
-                _startPosition.x + Random.Range(-5, 5),
-                _startPosition.y + Random.Range(-5, 5),
-                _startPosition.z
-                );
+            Vector3 newDestination = GetRandomPointInArea(_patrolArea);
+            Vector3 lookDirection = newDestination - transform.position;
+            float turnRate = 0.1f;
+            float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 
-            _agent.isStopped = false;
-            _agent.SetDestination(point);
+            transform.DORotate(new Vector3(0, 0, lookAngle), turnRate);
+            _agent.SetDestination(newDestination);
         }
+    }
+
+    private Vector3 GetRandomPointInArea(Rect area)
+    {
+        float randomX = Random.Range(area.xMin, area.xMax);
+        float randomY = Random.Range(area.yMin, area.yMax);
+
+        return new Vector3(randomX, randomY, 0);
     }
 }
--- Assets/Scripts/Enemy/StateMachine/PatrolTransition.cs
+++ Assets/Scripts/Enemy/StateMachine/PatrolTransition.cs
@@ -3,24 +3,5 @@ using UnityEngine;
 
 public class PatrolTransition : Transition
 {
-    private Coroutine _coroutine;
 
-    private void OnEnable()
-    {
-        _coroutine = StartCoroutine(Patroling());
-    }
-
-    private void OnDisable()
-    {
-        StopCoroutine(_coroutine);
-    }
-
-    private IEnumerator Patroling()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(Random.Range(5, 10));
-            NeedTransit = true;
-        }
-    }
 }
