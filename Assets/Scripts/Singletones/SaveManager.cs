29020921bc01a930119df7f28acab49d50ad3d7a
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2023-09-28 17:13:22 +0400

    1

--- Assets/Scripts/Singletones/SaveManager.cs
+++ Assets/Scripts/Singletones/SaveManager.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveManager
+public class SaveManager : MonoBehaviour
 {
     public static int CurrentLevel { get; private set; }
     public static float TotalScore { get; private set; }
 
     private const string Level = nameof(Level);
     private const string Score = nameof(Score);
+
+    public static SaveManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        CurrentLevel = PlayerPrefs.GetInt(Level);
+        TotalScore = PlayerPrefs.GetFloat(Score);
+    }
 
     public static void ResetSaves()
     {
